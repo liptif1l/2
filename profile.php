8c1a3c76df1f75870c08de36f6402f84c2364c2f
довгань <alinadovgan6@gmail.com>
2025-02-13 21:21:18 +0200

    profile.php

--- profile.php
+++ profile.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION["username"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Мій профіль</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="profile.css">
 </head>
 <body>
 
